557454b3ac717c35b6ae2c288de0a9b888db6521
Kakathic <kakathic@gmail.com>
2023-11-02 00:17:48 +0700

    Update Link.sh

--- .github/Link.sh
+++ .github/Link.sh
@@ -11,24 +11,59 @@ sudo rm -rf /usr/share/dotnet
 sudo rm -rf /opt/ghc
 sudo rm -rf /usr/local/share/boost
 
+# function
+Xem () { curl -s -G -L -N -H --connect-timeout 20 "$1"; }
+Taive () { curl -L -N -H --connect-timeout 20 -O "$1"; }
+checktc(){ grep -co 'dir="auto">.*'$1'' $TOME/1.ht 2>/dev/null; }
+
+# bot chat, thêm thẻ, đóng và chat, hủy chạy work, xoá thẻ 
+Chatbot(){ gh issue comment $NUMBIE --body "$1" & echo "$1"; }
+addlabel(){ gh issue edit $NUMBIE --add-label "$1"; }
+closechat(){ gh issue close $NUMBIE -c "$1"; }
+cancelrun(){ gh run cancel $GITHUB_RUN_ID; }
+removelabel(){ gh issue edit $NUMBIE --remove-label "$1"; }
+
 # get 
 Xem "https://github.com/Zelooooo/GLink/issues/$NUMBIE" > $TOME/1.ht
 
 # get url
 URL="$(grep -m1 'dir="auto">Url:' $TOME/1.ht | grep -o 'Url:.*<' | sed 's|Url:<||' | cut -d '"' -f2)"
 
+if [ "$URL" ];then
+Chatbot "Link detected: $URL"
+addlabel "Wait"
+else
+closechat "No link detected, stop process."
+addlabel "Error"
+removelabel "Wait"
+cancelrun
+fi
+
 # tao tm
 mkdir -p Up
 cd Up
 
-curl -L -N -H --connect-timeout 20 -O "$URL"
+Chatbot "Download to your device..."
+[ "$(echo "$URL" | grep -cm1 'mega.nz')" == 1 ] && SVD=1
+
+if [ "$SVD" = 1 ];then
+
+else
+Taive "$URL"
+fi
+
+# Tên file
 url1="$(ls)"
 
+Chatbot "Uploading files to the server..."
 if [ "$chsv" == 1 ];then
 
 else
 url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
 eval "curl -F 'file=@$url1' 'https://$url2.gofile.io/uploadFile' > $TOME/1.json"
-echo "Link download: $(cat $TOME/1.json | jq -r .data.downloadPage)"
+LinkDow="$(cat $TOME/1.json | jq -r .data.downloadPage)"
 fi
 
+removelabel "Wait"
+addlabel "Complete"
+closechat "Link download: $LinkDow"
