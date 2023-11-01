807137f1ed518d93f6b37f45078afbafeaf1319b
Kakathic <kakathic@gmail.com>
2023-11-01 23:44:39 +0700

    Update Link.sh

--- .github/Link.sh
+++ .github/Link.sh
@@ -3,3 +3,21 @@
 # install
 sudo apt-get install megatools
 
+TOME="$GITHUB_WORKSPACE"
+
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /opt/ghc
+sudo rm -rf /usr/local/share/boost
+mkdir -p Up
+cd Up
+curl -L -N -H --connect-timeout 20 -O "$FEATURE"
+url1="$(ls)"
+
+if [ "$chsv" == 1 ];then
+
+else
+url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
+eval "curl -F 'file=@$url1' 'https://$url2.gofile.io/uploadFile' > $TOME/1.json"
+echo "Link download: $(cat $TOME/1.json | jq -r .data.downloadPage)"
+fi
+
