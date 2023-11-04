48eca870eaea8103e0b6c4ab787ad32b59f5bf19
Kakathic <kakathic@gmail.com>
2023-11-04 13:16:36 +0700

    Update Link.sh

--- .github/Link.sh
+++ .github/Link.sh
@@ -42,7 +42,14 @@ Xem "https://github.com/Zelooooo/GLink/issues/$NUMBIE" > $TOME/1.ht
 URL="$(grep -m1 'dir="auto">Url:' $TOME/1.ht | grep -o 'Url:.*<' | sed 's|Url:<||' | cut -d '"' -f2)"
 
 # get sv
-chsv="$(checktc Transfer)"
+if [ "$(checktc Transfer)" == 1 ];then
+chsv=1
+elif [ "$(checktc Gofile)" == 1 ];then
+chsv=2
+else
+chsv=0
+fi
+
 
 if [ "$URL" ];then
 Chatbot "Link detected: $URL"
@@ -89,10 +96,12 @@ addtitle "Link Speed: $url1"
 # upload 
 if [ "$chsv" == 1 ];then
 LinkDow="$(curl --upload-file "$url1" https://transfer.sh)"
-else
+elif [ "$chsv" == 2 ];then
 url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
 eval "curl -F 'file=@$url1' 'https://$url2.gofile.io/uploadFile' > $TOME/1.json"
 LinkDow="$(cat $TOME/1.json | jq -r .data.downloadPage)"
+else
+LinkDow="$(eval "curl -X POST -F 'email=kakathic@gmail.com' -F 'key=xcjdJTOsvZJhgVV10B' -F 'file=@$url1' https://ul.mixdrop.ag/api" | jq -r .result.url)"
 fi
 
 # link download 
