3f76be96a758fd12cc978d73e69d9896d5911196
Kakathic <kakathic@gmail.com>
2023-11-10 16:37:44 +0700

    Update Link.sh

--- .github/Link.sh
+++ .github/Link.sh
@@ -92,7 +92,7 @@ if [ -e "$TOME/bug.txt" ];then
 [ -e "$TOME/chat" ] || Chatbot "Calculate loading speed..."
 echo > $TOME/chat
 sleep 1
-chatbotedit "$(tail -c80 bug.txt | awk '{print "Total: "$3" Loaded: "$5" Speed: "$13"b"}')"
+[ "$(echo "$URL" | grep -cm1 'mega.nz')" ] && chatbotedit "$(tail -c80 $TOME/bug.txt)" || chatbotedit "$(tail -c80 $TOME/bug.txt | awk '{print "Total: "$3" Loaded: "$5" Speed: "$13"b"}')"
 fi
 done
 )
