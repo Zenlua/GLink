cdd78895b86c43f96941b2f46fca353dff9eb9e5
Kakathic <kakathic@gmail.com>
2023-11-13 00:25:01 +0700

    Update Link.sh

--- .github/Link.sh
+++ .github/Link.sh
@@ -94,9 +94,9 @@ if [ "$(echo "$URL" | grep -cm1 'mega.nz')" == 1 ];then
 chatbotedit "$(tail -n1 $TOME/bug.txt)"
 else
 chatbotedit "$(tail -c80 $TOME/bug.txt | awk '{print "Total: "$3" Loaded: "$5" Speed: "$13"b"}')"
-checksp="$(tail -c80 $TOME/bug.txt | awk '{print $5$13}')"
+checksp="$(tail -c80 $TOME/bug.txt | awk '{print $13}')"
 sleep 1
-[ "$(tail -c80 $TOME/bug.txt | awk '{print $5$13}')" == "$checksp" ] && jsdhhd="$(($jsdhhd + 1))"
+[ "$(tail -c80 $TOME/bug.txt | awk '{print $13}')" == "$checksp" ] && jsdhhd="$(($jsdhhd + 1))"
 [ "$jsdhhd" -ge 10 ] && bug 'Error: Network problem try again tomorrow.'
 fi
 done
