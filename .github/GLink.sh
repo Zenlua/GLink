07827e936e3288225ab06e8bfc29a02eb11ff8cd
Kakathic <kakathic@gmail.com>
2024-01-03 08:06:19 +0700

    Update GLink.sh

--- .github/GLink.sh
+++ .github/GLink.sh
@@ -54,6 +54,7 @@ fi
 
 if [ "$URL" ];then
 addlabel "Wait" & removelabel "Error"
+Chatbot "Start downloading the file..."
 else
 bug 'No link detected, stop process. Note, please leave the `Url: ` if you remove the Url it will not receive the link.'
 fi
@@ -62,7 +63,6 @@ fi
 mkdir -p Up
 cd Up
 
-(
 # phát hiện sv download & tải về 
 if [ "$(echo "$URL" | grep -cm1 'mega.nz')" == 1 ];then
 sudo apt-get install megatools >/dev/null 2>/dev/null
@@ -70,62 +70,26 @@ megadl "$URL" 2>&1 | tee "$TOME/bug.txt"
 elif [ "$(echo "$URL" | grep -cm1 'sourceforge.net')" == 1 ];then
 mausourcef="$(echo "$URL" | cut -d '/' -f 5,7)"
 tensourcef="$(echo "$URL" | cut -d '/' -f 10)"
-Taive2 "${URL}?use_mirror=zenlayer&r=" "$tensourcef" 2>&1 | tee "$TOME/bug.txt"
+Taive2 "${URL}?use_mirror=zenlayer&r=" "$tensourcef" 2>&1"
 else
-Taive "$URL" 2>&1 | tee "$TOME/bug.txt"
+Taive "$URL" 2>&1"
 fi
-echo > "$TOME/done"
-) & 
-# Hiện tốc độ và check close
-jsdhhd=0
-while true; do
-if [ "$(gh issue view $NUMBIE | grep -cm1 CLOSED)" == 1 ];then
-bug "The order to cancel the process has been received."
-sleep 100
-fi
-if [ ! -e "$TOME/chat" ];then
-Chatbot "Calculate loading speed..."
-echo > $TOME/chat
-sleep 2
-fi
-sleep 1
-[ -e "$TOME/done" ] && break || ffngnw="$(($ffngnw + 1))"
-[ "$(grep -cm1 'API rate limit' $TOME/log.txt 2>/dev/null)" == 1 ] && bug 'The download speed has exceeded the allowable limit. Please download tomorrow.'
-[ "$ffngnw" -ge 4000 ] && bug '60 minute download limit has expired, canceling the run.'
-if [ "$(echo "$URL" | grep -cm1 'mega.nz')" == 1 ];then
-chatbotedit "$(tail -n1 $TOME/bug.txt)"
-else
-chatbotedit "$(tail -c80 $TOME/bug.txt | awk '{print "Total: "$3" Loaded: "$5" Speed: "$13"b"}')"
-checksp="$(tail -c80 $TOME/bug.txt | awk '{print $13}')"
-fi
-done
-
 
 # Tên file
 url1="$(ls)"
 echo "- Name: $url1"
-[[ -e "$url1" ]] && chatbotedit "Uploading files to the server..." || bug "Download file not found, download error.<br/><br/>$(cat "$TOME/bug.txt")"
-sleep 1
+chatbotedit "Uploading files to the server..."
 addtitle "Link Speed: $url1"
 
 # upload 
-(
 if [ "$chsv" == 1 ];then
 curl --upload-file "$url1" https://transfer.sh > $TOME/1.json
 else
 url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
-eval "curl --dns-servers '1.1.1.1,8.8.8.8' -L -N -H '$User' -F 'file=@$url1' 'https://$url2.gofile.io/uploadFile' 2>&1 > $TOME/1.json"
+eval "curl --dns-servers '1.1.1.1,8.8.8.8' -L -N -H '$User' -F 'file=@$url1' 'https://$url2.gofile.io/uploadFile' 2>&1 | tee $TOME/1.json"
 #LinkDow="$(eval "curl -X POST -F 'email=kakathic@gmail.com' -F 'key=xcjdJTOsvZJhgVV10B' -F 'file=@$url1' -F 'folder=821972' https://ul.mixdrop.ag/api" | jq -r .result.url)"
 fi
-echo > $TOME/bone
-) & (
-stken=0
-while true; do
-[ -e "$TOME/bone" ] && break || stken="$(($stken + 1))"
-sleep 1
-[ "$stken" -ge 2000 ] && bug '30 minute upload limit has expired, canceling the run.'
-done
-)
+
 
 # link download 
 if [ "$(cat $TOME/1.json)" ];then
