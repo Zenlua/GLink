e9791e15c65cf9e767fb620b7a2eff7837d7b48c
Kẹo Kéo <kakathic@gmail.com>
2024-05-17 23:18:18 +0700

    Update DLink.sh

--- .github/DLink.sh
+++ .github/DLink.sh
@@ -43,7 +43,7 @@ echo > $TOME/tc.log
 res_json=$(curl -s -X GET "https://devuploads.com/api/upload/server?key=47395exzbd07av0fozl8h")
 sess_id=$(echo "$res_json" | grep -o '"sess_id":"[^"]*"' | awk -F ':' '{print $2}' | tr -d '"')
 server_url=$(echo $res_json | sed -n 's/.*"result":"\([^"]*\).*/\1/p')
-curl --dns-servers '1.1.1.1' -s -X POST -F "sess_id=$sess_id" -F "utype=reg" -F "file=@$url1" "$server_url" | jq
+#curl --dns-servers '1.1.1.1' -s -X POST -F "sess_id=$sess_id" -F "utype=reg" -F "file=@$url1" "$server_url" | jq
 )
 
 while true; do
