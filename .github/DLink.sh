1efc6d2d27efc148e4f5cf0ff9ae1c05a67f0be4
Kẹo Kéo <kakathic@gmail.com>
2024-05-18 00:05:19 +0700

    Update DLink.sh

--- .github/DLink.sh
+++ .github/DLink.sh
@@ -40,7 +40,7 @@ else
 url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
 [ -z "$url2" ] && url2="store$(( $RANDOM % 8 + 1 ))"
 eval "curl --dns-servers '1.1.1.1' -L -N -H '$User' -F 'file=@$url1' 'https://store'$url2'.gofile.io/uploadFile'" | jq
-echo > $TOME/tc.log
+echo "$url2" | tee $TOME/tc.log
 ) &
 res_json=$(curl -s -X GET "https://devuploads.com/api/upload/server?key=47395exzbd07av0fozl8h")
 sess_id=$(echo "$res_json" | grep -o '"sess_id":"[^"]*"' | awk -F ':' '{print $2}' | tr -d '"')
