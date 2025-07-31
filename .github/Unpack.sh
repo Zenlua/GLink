c943d14540e08ac66aeaff06d87e3aa79195cf73
Kakathic <kakathic@gmail.com>
2025-07-31 16:40:28 +0700

    Update Unpack.sh

--- .github/Unpack.sh
+++ .github/Unpack.sh
@@ -10,6 +10,14 @@ ls bin
 
 gettype -i $TOME/.github/mi_ext.img
 
+Taiveeu(){
+mausourcef="$(echo "$1" | cut -d '/' -f 5,7)"
+tensourcef="$(echo "$1" | cut -d '/' -f 10)"
+Taive2 "${1}?use_mirror=zenlayer&r=" "$tensourcef" 2>&1
+}
+
+
+
 echo
 env
 
