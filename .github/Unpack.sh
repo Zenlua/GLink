a1039d54837bde9d96bce250d853d3792872050c
Kakathic <kakathic@gmail.com>
2025-07-31 16:38:18 +0700

    Update Unpack.sh

--- .github/Unpack.sh
+++ .github/Unpack.sh
@@ -1,9 +1,11 @@
 # Kakathic
-curl -L https://github.com/althafvly/AmlogicKitchen/archive/refs/heads/master.zip \
+curl -sL https://github.com/althafvly/AmlogicKitchen/archive/refs/heads/master.zip \
 -o mod.zip
 unzip -qoj mod.zip */bin/* -d bin
 PATH=bin:$PATH
 TOME="$GITHUB_WORKSPACE"
+MOME="$TOME/.github"
+
 ls bin
 
 gettype -i $TOME/.github/mi_ext.img
